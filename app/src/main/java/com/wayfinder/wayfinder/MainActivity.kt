82ef0274061bb20b9172736402ccd402f085967d
bb-ankurpandey <ankur.pandey1@bigbasket.com>
2024-03-11 23:05:30 +0530

    Initial commit

---
+++ app/src/main/java/com/wayfinder/wayfinder/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.wayfinder.wayfinder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Check if the fragment is already present
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MapFragment())
+                .commit()
+        }
+    }
+}
---
+++ app/src/main/java/com/wayfinder/wayfinder/MapFragment.kt
@@ -0,0 +1,37 @@
+package com.wayfinder.wayfinder
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+
+class MapFragment : Fragment(), OnMapReadyCallback {
+
+    private lateinit var mMap: GoogleMap
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // Initialize the map. Make sure the ID matches the one in your fragment's layout.
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(this)
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Now that the map is ready, you can use mMap to customize your map
+        // For example, setting the map type, adding markers, moving the camera, etc.
+    }
+}
