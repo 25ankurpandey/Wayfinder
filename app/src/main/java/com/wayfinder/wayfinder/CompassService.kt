d1bc97b294c54ea3b0638afb135aef6c06c090bd
bb-ankurpandey <ankur.pandey1@bigbasket.com>
2024-03-26 23:31:43 +0530

    Navigation updated

---
+++ app/src/main/java/com/wayfinder/wayfinder/CompassService.kt
@@ -0,0 +1,62 @@
+package com.wayfinder.wayfinder
+
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+
+class CompassService(context: Context) : SensorEventListener {
+    private var listener: CompassListener? = null
+    private val sensorManager = context.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+    private val accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)
+    private val magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)
+    private var lastAccelerometer = FloatArray(3)
+    private var lastMagnetometer = FloatArray(3)
+    private var isAccelerometerSet = false
+    private var isMagnetometerSet = false
+    private var r = FloatArray(9)
+    private var orientation = FloatArray(3)
+
+    private val gravity = FloatArray(3)
+    private val geomagnetic = FloatArray(3)
+    private var azimuth = 0f // Rotation around the Z axis
+
+    interface CompassListener {
+        fun onNewAzimuth(azimuth: Float)
+    }
+
+    fun start() {
+        // Change SENSOR_DELAY_GAME to SENSOR_DELAY_NORMAL for a slower update rate
+        sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL)
+        sensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_NORMAL)
+    }
+
+
+    fun stop() {
+        sensorManager.unregisterListener(this)
+    }
+
+    fun setCompassListener(listener: CompassListener) {
+        this.listener = listener
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {}
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, lastAccelerometer, 0, event.values.size)
+            isAccelerometerSet = true
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, lastMagnetometer, 0, event.values.size)
+            isMagnetometerSet = true
+        }
+        if (isAccelerometerSet && isMagnetometerSet) {
+            SensorManager.getRotationMatrix(r, null, lastAccelerometer, lastMagnetometer)
+            SensorManager.getOrientation(r, orientation)
+            val azimuthInRadians = orientation[0]
+            val azimuthInDegrees = (Math.toDegrees(azimuthInRadians.toDouble()).toFloat() + 360) % 360
+            listener?.onNewAzimuth(azimuthInDegrees)
+        }
+    }
+}
--- app/src/main/java/com/wayfinder/wayfinder/NavigationFragment.kt
+++ app/src/main/java/com/wayfinder/wayfinder/NavigationFragment.kt
@@ -9,21 +9,24 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.lifecycleScope
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.LatLngBounds
+import com.google.android.gms.maps.model.Marker
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.android.gms.maps.model.Polyline
 import com.google.android.gms.maps.model.PolylineOptions
 import com.google.gson.Gson
 import com.google.maps.android.PolyUtil
+import com.google.maps.android.SphericalUtil
 import com.wayfinder.wayfinder.MapConstants.SELECTED_POLYLINE_WIDTH
 import com.wayfinder.wayfinder.databinding.FragmentNavigationBinding
 import com.wayfinder.wayfinderar.LocationService
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class NavigationFragment : Fragment(), OnMapReadyCallback {
@@ -33,11 +36,14 @@ class NavigationFragment : Fragment(), OnMapReadyCallback {
     private lateinit var mMap: GoogleMap
     private lateinit var locationService: LocationService
     private var selectedRouteJson: String? = null
+    private var userLocationMarker: Marker? = null
+    private var currentPolyline: Polyline? = null
+    private var remainingPath: MutableList<LatLng> = mutableListOf()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentNavigationBinding.inflate(inflater, container, false)
         locationService = LocationService(requireContext())
-        selectedRouteJson = arguments?.getString("selectedRouteJson") // Changed to accept the whole route JSON
+        selectedRouteJson = arguments?.getString("selectedRouteJson")
         return binding.root
     }
 
@@ -53,86 +59,104 @@ class NavigationFragment : Fragment(), OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
-        fetchLocationAndDisplay()
-        setupMapWithRoute()
+        initializeLocationUpdates()
     }
 
-    private fun setupMapWithRoute() {
-        lifecycleScope.launch {
-            val lastLocation = locationService.getLastLocation()
-            lastLocation?.let { location ->
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-                // Add a custom marker at the user's current location
-                mMap.addMarker(MarkerOptions()
-                    .position(currentLatLng)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_user_location))) // Ensure you have this drawable in your resources
-
-                selectedRouteJson?.let {
-                    val route = Gson().fromJson(it, Route::class.java) // Assuming Route is your route class with a proper overview_polyline property
-                    val decodedPath = PolyUtil.decode(route.overview_polyline.points)
-
-                    // Prepend the current location to the decoded path
-                    val fullPath = mutableListOf(currentLatLng).apply {
-                        addAll(decodedPath)
-                    }
-
-                    val polylineOptions = PolylineOptions()
-                        .addAll(fullPath)
-                        .color(ContextCompat.getColor(requireContext(), R.color.selectedPolylineColor))
-                        .width(SELECTED_POLYLINE_WIDTH.toFloat())
-                    mMap.addPolyline(polylineOptions)
-
-                    // Focus camera on the user's current location with appropriate zoom
-                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, getZoomLevelForOneKilometerCoverage(currentLatLng)))
-
-                    // Add marker for the destination
-                    mMap.addMarker(MarkerOptions().position(decodedPath.last()).title("Destination"))
-                }
-            } ?: Log.d("NavigationFragment", "Last location is null")
+    private fun initializeLocationUpdates() {
+        locationService.startLocationUpdates(1000L, 500L) { locationResult ->
+            val newLocation = locationResult.lastLocation?.let { LatLng(it.latitude, locationResult.lastLocation!!.longitude) }
+            if (newLocation != null) {
+                updateLocationOnMap(newLocation)
+            }
         }
     }
 
-    private fun fetchLocationAndDisplay() {
-        lifecycleScope.launch {
-            val lastLocation = locationService.getLastLocation()
-            lastLocation?.let { location ->
-                val currentLatLng = LatLng(location.latitude, location.longitude)
-
-                // Add a custom marker at the user's current location
-                mMap.addMarker(MarkerOptions()
-                    .position(currentLatLng)
-                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_user_location))) // Make sure you have this drawable
-
-                // Apply padding to the map view. Adjust only the bottom padding.
-                val padding = getMapPaddingBottom()
-                mMap.setPadding(0, 0, 0, padding)
-
-                // Animate camera to the user's location with appropriate zoom
-                // Zoom level 15f is arbitrary, adjust as necessary
-                mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 19f))
-            } ?: Log.d("NavigationFragment", "Last location is null")
+    private fun initializeRemainingPath() {
+        // This should be called after getting the route and user's initial location
+        selectedRouteJson?.let {
+            val route = Gson().fromJson(it, Route::class.java)
+            remainingPath = PolyUtil.decode(route.overview_polyline.points).toMutableList()
         }
     }
 
-    // This method is an approximation and may need adjustments based on testing
+    private fun findClosestPointOnPath(currentLocation: LatLng, path: List<LatLng>): Int {
+        var minIndex = -1
+        var minDistance = Double.MAX_VALUE
+        for (i in path.indices) {
+            val distance = SphericalUtil.computeDistanceBetween(currentLocation, path[i])
+            if (distance < minDistance) {
+                minDistance = distance
+                minIndex = i
+            }
+        }
+        return minIndex
+    }
+
+
+    private fun updateLocationOnMap(newLocation: LatLng) {
+        // Ensure the remaining path is initialized
+        if (remainingPath.isEmpty()) {
+            initializeRemainingPath()
+        }
+
+        // Find the closest point on the remaining path to the new location
+        val closestPointIndex = findClosestPointOnPath(newLocation, remainingPath)
+        if (closestPointIndex > 0) {
+            // Trim the traveled part of the path
+            remainingPath = remainingPath.subList(closestPointIndex, remainingPath.size).toMutableList()
+        }
+
+        // Update user's location marker
+        userLocationMarker?.remove() // Remove the old marker
+        userLocationMarker = mMap.addMarker(MarkerOptions()
+            .position(newLocation)
+            .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_user_location)))
+
+        // Redraw the polyline
+        currentPolyline?.remove()
+        if (remainingPath.isNotEmpty()) {
+            currentPolyline = mMap.addPolyline(PolylineOptions()
+                .addAll(remainingPath)
+                .color(ContextCompat.getColor(requireContext(), R.color.selectedPolylineColor))
+                .width(SELECTED_POLYLINE_WIDTH.toFloat()))
+        }
+
+        // Adjust the camera
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(newLocation, getZoomLevelForOneKilometerCoverage(newLocation)))
+    }
+
+    private suspend fun setupMapWithRoute(newLocation: LatLng) {
+        selectedRouteJson?.let {
+            val route = Gson().fromJson(it, Route::class.java)
+            val decodedPath = PolyUtil.decode(route.overview_polyline.points)
+
+            // If a polyline exists, remove it
+            currentPolyline?.remove()
+
+            // Create a new polyline that starts from the user's current location
+            currentPolyline = mMap.addPolyline(PolylineOptions()
+                .add(newLocation)
+                .addAll(decodedPath)
+                .color(ContextCompat.getColor(requireContext(), R.color.selectedPolylineColor))
+                .width(SELECTED_POLYLINE_WIDTH.toFloat()))
+
+            // Focus camera on the user's current location
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(newLocation, getZoomLevelForOneKilometerCoverage(newLocation)))
+        }
+    }
+
+    // This method calculates the appropriate zoom level to cover approximately 1 kilometer
     private fun getZoomLevelForOneKilometerCoverage(center: LatLng): Float {
-        // Rough approximation - you might need to adjust the base zoom level based on testing
-        val baseZoomLevel = 18f // This zoom level is a starting point and might show approximately 1km in urban areas
-        return baseZoomLevel
+        return 18f // Adjust based on testing and requirements
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
+        locationService.stopLocationUpdates() // Ensure to stop location updates
         _binding = null
     }
 
-    private fun dpToPx(context: Context, dp: Float): Int {
-        return (dp * context.resources.displayMetrics.density).toInt()
-    }
-
-    private fun getMapPaddingBottom(): Int {
-        // Adjust this value if you want to change the distance from the bottom
-        val distanceFromBottomDp = 200f
-        return dpToPx(requireContext(), distanceFromBottomDp)
+    private fun dpToPx(dp: Float): Int {
+        return (dp * resources.displayMetrics.density).toInt()
     }
 }
